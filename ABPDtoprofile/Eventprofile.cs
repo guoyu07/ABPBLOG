30cf3962bc95750b19ffbdeb42e36ff11ba91795
enjoysun <1570819807@qq.com>
2017-03-20 16:16:03 +0800

    领域模型实现user贫血

---
+++ ABPDtoprofile/Eventprofile.cs
@@ -0,0 +1,29 @@
+﻿using ABPLOGDTO;
+using ABPLOGMODEL;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABPDtoprofile
+{
+    public class Eventprofile
+    {
+        public void Usermapper()
+        {
+            Mapper.Initialize(x => x.CreateMap<User, Uerdto>()
+             .ForMember(dest => dest.user_name_dto, opt => opt.MapFrom(c => c.user_name))
+             .ForMember(dest => dest.user_pwd_dto, opt => opt.MapFrom(c => c.user_pwd))
+             .ForMember(dest => dest.user_phone_dto, opt => opt.MapFrom(c => c.user_phone))
+             .ForMember(dest => dest.user_sex_dto, opt => opt.MapFrom(c => c.user_sex))
+             .ForMember(dest => dest.user_email_dto, opt => opt.MapFrom(c => c.user_email))
+             .ForMember(dest => dest.user_last_login_ip_dto, opt => opt.MapFrom(c => c.user_last_login_ip))
+             .ForMember(dest => dest.user_description_dto, opt => opt.MapFrom(c => c.user_description))
+             .ForMember(dest => dest.user_image_url_dto, opt => opt.MapFrom(c => c.user_image_url))
+             .ForMember(dest => dest.user_register_time_dto, opt => opt.MapFrom(c => c.user_register_time))
+            );
+        }
+    }
+}
---
+++ ABPLOGDTO/Uerdto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABPLOGDTO
+{
+    public class Uerdto
+    {
+        public string user_name_dto { get; set; }
+        public string user_pwd_dto { get; set; }
+        public string user_phone_dto { get; set; }
+        public bool user_sex_dto { get; set; }
+        public string user_email_dto { get; set; }
+        public string user_last_login_ip_dto { get; set; }
+        public string user_description_dto { get; set; }
+        public string user_image_url_dto { get; set; }
+        public DateTime user_register_time_dto { get; set; }
+    }
+}
