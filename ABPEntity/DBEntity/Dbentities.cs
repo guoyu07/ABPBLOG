aea94ee0709b6ea8c445fe0c99a1fe88690cb6b0
Administrator <Administrator@USER-20160219YB>
2017-04-10 13:28:22 +0800

    修改为codefrist模式

---
+++ ABPEntity/DBEntity/Dbentities.cs
@@ -0,0 +1,19 @@
+﻿using ABPEntity.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABPEntity.DBEntity
+{
+    public class Dbentities : DbContext
+    {
+        public Dbentities() : base("name=ABPBLOG")
+        {
+
+        }
+        public DbSet<ABPUser> userinfo { get; set; }
+    }
+}
---
+++ ABPEntity/Model/ABPUser.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABPEntity.Model
+{
+    public class ABPUser
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int user_id { get; set; }
+        public int group_id { get; set; }
+        public string user_name { get; set; }
+        public string user_pwd { get; set; }
+        public string user_phone { get; set; }
+        public bool user_sex { get; set; }
+        public string user_qq { get; set; }
+        public string user_email { get; set; }
+        public string user_address { get; set; }
+        public int user_mark { get; set; }
+        public int user_rank_id { get; set; }
+        public string user_last_login_ip { get; set; }
+        public DateTime user_birthday { get; set; }
+        public string user_description { get; set; }
+        public string user_image_url { get; set; }
+        public DateTime user_register_time { get; set; }
+        public bool user_lock { get; set; }
+        public bool user_freeze { get; set; }
+        public string user_power { get; set; }
+    }
+}
